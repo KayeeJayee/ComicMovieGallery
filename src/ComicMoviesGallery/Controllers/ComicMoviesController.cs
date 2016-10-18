1323694b5593c355d429dd0ee598e50796e75d2f
KathrynMillar <kathrynart.artist@gmail.com>
2016-10-19 11:11:37 +1300

    Added ComicMoviesController

---
+++ src/ComicMoviesGallery/Controllers/ComicMoviesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicMoviesGallery.Controllers
+{
+    public class ComicMoviesController : Controller
+    {
+        public string Detail()
+        {
+            return "Hello World";
+        }
+    }
+}
